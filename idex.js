fcba1e3adea5e657a570d8faf0a860a82d01ea5b
Seyran Kerimov <141037012+SeyranKerimov@users.noreply.github.com>
2024-01-19 04:30:44 +0400

    java add

---
+++ idex.js
@@ -0,0 +1,58 @@
+// const user ={
+//     name: 'seyran'
+// }
+// console.log(user)
+
+///////////////////////////////////YOXALAMA 1
+
+// var name = 'seyran'
+// var surname = 'kerimov'
+// var age = 23
+
+// console.log(age)
+////////////////////////////////////////YOXLAMA 2
+
+// const personalProfil ={
+//     firstName:'seyran',
+//     lastName: 'kerimov',
+//     age: 20,
+//     favoriteLanguage:'React js',
+//     contactInfo:{
+//         email:'seyrankerimov12@mail.com',
+//         phone:'+994519842888'
+//     }
+// };
+
+// // console.log(
+// //     "Private Personal Profile:",personalProfil
+// // )
+
+// personalProfil["age"]="seyran";
+
+// personalProfil.contactInfo["address"]="Heydar Aliyev street";
+// console.log(personalProfil)
+
+
+///////////////////YOXLAMA 3
+
+// const user ={
+//     name : 'seqo',
+//     surname : 'kerimov', 
+//     age : 34,
+//     father: 'telman',
+//     contactInfo1:{
+//         email:'seyranebilov002@gmail.com',
+//         phone1:'0519842888',
+//         address1 : 'sulh 1a',
+//         text:{
+//            write: 'SALAM ALEYKUMEE'
+//         }
+//     }
+// }
+// user['name']='tello';
+// user.contactInfo1.text["raast"]="salam qaqas";
+// console.log(user)
+
+
+
+///////////////////////// YOXLAMA 4
